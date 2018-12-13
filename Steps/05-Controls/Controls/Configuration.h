5a1cd621cf6fc82d87bd6371e7cc74778ae531a1
Alvaro García Loaisa <alvaro.garcia@osoco.es>
2018-12-13 22:42:09 +0100

    config file

--- Steps/05-Controls/Controls/Configuration.h
+++ Steps/05-Controls/Controls/Configuration.h
@@ -11,6 +11,10 @@ const int STATUS_STAND_BY = -1;
 const int STATUS_INIT = 0;
 const int STATUS_ACTIVATE_GAME = 1;
 const int STATUS_WAITING_RELEASE_COIN = 2;
+const int STATUS_WIN_OR_LOST = 3;
+const int STATUS_WIN = 4;
+const int STATUS_LOST = 5;
+const int STATUS_OFF = 6;
 
 //////////////
 // PINS     //
@@ -32,6 +36,18 @@ const int CONTROLS_HORIZONTAL_CLK_PIN = 26;
 const int SERVO_HORIZONTAL_PIN = 27;
 
 const int SOLENOID_PIN = 31;
+const int FREE_COIN_BUTTON_PIN = 33;
+
+const int SENSOR_WIN_PIN = 35;
+
+const int MOTOR_ENABLE_PIN = 39;
+const int MOTOR_WIN_STEP_PIN = 41;
+const int MOTOR_WIN_DIR_PIN = 43;
+const int SWITCH_WIN_CARD_MOTOR_PIN = 45;
+
+const int MOTOR_LOST_STEP_PIN = 40;
+const int MOTOR_LOST_DIR_PIN = 42;
+const int SWITCH_LOST_CARD_MOTOR_PIN = 44;
 
 /////////////////////
 // MUSIC AND SOUND //
@@ -52,6 +68,14 @@ const int DELAY_AFTER_INSERT_COIN = 3000;
 
 const int POINT_TO_MOUTH_WAIT_TIME = 25000;
 const int MAKE_WITH_WAIT_TIME = 5000;
+
+const int FREE_COIN_TIME = 10000;
+
+const int WAIT_LOST_TIME = 5000;
+
+const int MOTOR_STEP_VELOCITY_DELAY = 50;
+
+const int GAME_TOTAL_TIME_FINISH = 10000;
 
 ///////////////////////////
 // CONTROLS SERVO LIMITS //
